cdd779ef56f10a2be86d5ea1d705aeb41699fa04
Oliver Stigley <oliverstigley@gmail.com>
2016-06-09 17:33:11 +1200

    tests set up before pull react

---
+++ test/components/components.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils'
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+
+import App from '../../components/app'
+import Products from '../../Homepage-product'
+import Product from '../../components/Indiv-product'
+
+const {
+  isCompositeComponent,
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithClass,
+  scryRenderedDOMComponentsWithTag,
+  Simulate
+} = TestUtils
+
+describe('The homepage component', () => {
+	const homepage = mount(<Products/>)
+	it('contains an h1 tag', () => {
+		expectation(homepage.contains(<IndividualProduct />)).to.equal(true)
+	})
+
+})
+
+describe('The Indiv-product component', () => {
+	const homepage = shallow(<Products/>)
+	it('contains an h1 tag', () => {
+		expectation(homepage.find('h1').length).to.equal(1)
+	})
+
+	it('contains an img tag', () => {
+		expectation(homepage.find('img').length).to.equal(1)
+	})
+
+	it('contains an p tag', () => {
+		expectation(homepage.find('p').length).to.equal(1)
+	})
+
+	it('contains an btn tag', () => {
+		expectation(homepage.find('btn').length).to.equal(1)
+	})
+
+})
