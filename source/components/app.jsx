0914fbd37532d183deceafee30515e1eeb6c83f9
Oliver Stigley <oliverstigley@gmail.com>
2016-06-10 11:19:02 +1200

    initial

--- source/components/app.jsx
+++ source/components/app.jsx
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import HomeProductsIndex from './HomeProductsIndex.jsx'
 
 class App extends Component {
+	componentWillMount() {
+		$.ajax({
+			url: '/username',
+			type: 'GET',
+			suceess: this.updateUser
+		})
+	}
+
+	updateUser(data) {
+		this.setState({user: data})
+	}
 
   render () {
     return (
     	<div>
     		<h1>Welcome to {this.props.name}</h1>
+    		<p>Your name is {this.state.user} </p>
         <HomeProductsIndex />
 		</div>
     )	
