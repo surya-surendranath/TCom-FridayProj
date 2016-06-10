5b3e727957f4537c1aa5808d3fb62b2edd13d012
Oliver Stigley <oliverstigley@gmail.com>
2016-06-10 15:52:14 +1200

    mulitple items showing in cart

--- source/components/cart.jsx
+++ source/components/cart.jsx
@@ -7,18 +7,31 @@ import React from 'react'
 export default ({cart, products}) => {
 
 	const getProd = () => {
+		var arry = []
+		for ( var ids in cart) {
+			arry.push((<li>{products[ids].title}, qty: {cart[ids]}, total-cost: ${Number(products[ids].price)*cart[ids]}</li>))
+		}
+		console.log(arry)
+		return arry
+	}
+
+	const total = () => {
+		let total = 0
 		for ( var ids in cart) {
 			// console.log(Number(products[ids].price))
-			return (<li>{products[ids].title}, qty: {cart[ids]}, total-cost: ${Number(products[ids].price)*cart[ids]}</li>)
+			total+=Number(products[ids].price)*cart[ids]
 		}
+		return total
 	}
+	
 
 
 	return (
 	<div id='cart'>
 		<h4>This is your shopping cart</h4>
-		<ul>{getProd()}
+		<ul>{getProd().map( (item) => item )}
 		</ul>
+	<p>Total: ${Math.floor(total())}</p>
 		
 	</div>)
 }
